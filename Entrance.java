9db6952d9973156fc6b52c5bedb7c9e5b2ade61b
Stang Win PC <ittiphat.ch@outlook.com>
2020-04-23 21:34:01 +0700

    - Add Java Demo Code

---
+++ Entrance.java
@@ -0,0 +1,7 @@
+public class Entrance
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to smartmeal");
+        System.out.println("Please Input User and Password : ");
+    }
+}
